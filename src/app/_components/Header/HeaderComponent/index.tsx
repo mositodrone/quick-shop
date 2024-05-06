f55ab68a86cc735acf2043b8003c9f47631ddca7
BigAtom <mositodrone@gmail.com>
2024-05-06 03:11:32 +0100

    Update Header Component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -8,7 +8,6 @@ import { usePathname } from 'next/navigation'
 import { Header } from '../../../../payload/payload-types'
 import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
-// import MobileNav from '../MobileNav'
 import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
@@ -28,7 +27,6 @@ const HeaderComponent = ({ header }: { header: Header }) => {
         </Link>
 
         <HeaderNav header={header} />
-        {/* <MobileNav header={header} /> */}
       </Gutter>
     </nav>
   )
